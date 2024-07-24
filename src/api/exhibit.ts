0da946bf180ac73b7967ea247e77618aab805cf9
mokyoungg <mokyoungg@gmail.com>
2024-07-24 20:47:28 +0900

    feat: update getExhibit api, call with pageparam

--- src/api/exhibit.ts
+++ src/api/exhibit.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const getExhibit = async () => {
+export const getExhibit = async (pageParam: number = 1) => {
   const response = await axios.get(
     `http://api.kcisa.kr/openapi/CNV_060/request`,
     {
       params: {
-        serviceKey: process.env.API_KEY,
+        serviceKey: process.env.NEXT_PUBLIC_API_KEY,
         numOfRows: 10,
-        pageNo: 1,
+        pageNo: pageParam,
       },
     }
   );
